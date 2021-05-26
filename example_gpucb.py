2967860bd76b7d8aa37eaf64aad4fe004b690b1f
Joel Lidin <joellidin@gmail.com>
2021-05-26 19:37:59 +0200

    Updated example

--- example_gpucb.py
+++ example_gpucb.py
@@ -4,12 +4,16 @@ from thompson_sampling import GaussianProcessesStrategy, ThompsonSampling
 
 
 def fitness(params: np.ndarray):
-    return np.exp(-(params ** 2)), params
+    reward = np.exp(-(params ** 2)) + np.cos(params) + np.random.normal()
+    return reward, params
 
 
 def on_update(epoch, strategy, last_epoch):
     if epoch % 10 == 0:
         print("Epoch {} - Current sample {}".format(epoch, strategy.sample()))
+    if last_epoch:
+        print("SEARCH DONE")
+        print(strategy.sample())
     return False
 
 
@@ -17,9 +21,9 @@ if __name__ == "__main__":
     strategy = GaussianProcessesStrategy(1, np.arange(-10, 10, 0.1), 1.0)
     ts = ThompsonSampling(
         thompson_strategy=strategy,
-        epochs=500,
+        epochs=200,
         fitness_function=fitness,
         callbacks={"on_update": on_update},
+        num_processors=1,
     )
     strategy = ts.run()
-    print(strategy.sample())
